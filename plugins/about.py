d4a075a0e6a3ee0cdfda2553315e48c621005a03
dhanush2bot <149936819+dhanush2bot@users.noreply.github.com>
2023-11-18 08:53:39 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -1,13 +1,33 @@
-import os 
+import os
 from pyrogram import Client, filters
-token = os.environ.get('TOKEN','')
-botid = token.split(':')[0]
 from helper.database import botdata, find_one, total_user
 from helper.progress import humanbytes
+
+token = os.environ.get('TOKEN', '')
+botid = token.split(':')[0]
+
 @Client.on_message(filters.private & filters.command(["about"]))
-async def start(client,message):
-	botdata(int(botid))
-	data = find_one(int(botid))
-	total_rename = data["total_rename"]
-	total_size = data["total_size"]
-	await message.reply_text(f"Total User:- {total_user()}\nBot :- @rename_urbot\nCreater :- @mrlokaman\nLanguage :-Python3\nLibrary :- Pyrogram 2.0\nServer :- Railway\nTotal Renamed File :-{total_rename}\nTotal Size Renamed :- {humanbytes(int(total_size))} ",quote=True)
+async def start(client, message):
+    botdata(int(botid))
+    data = find_one(int(botid))
+
+    try:
+        total_rename = data["total_rename"]
+        total_size = data["total_size"]
+    except KeyError as e:
+        # Handle the KeyError, set default values, or take appropriate action
+        total_rename = 0
+        total_size = 0
+        print(f"KeyError: {e}")
+
+    await message.reply_text(
+        f"Total User:- {total_user()}\n"
+        f"Bot :- @rename_urbot\n"
+        f"Creator :- @mrlokaman\n"
+        f"Language :- Python3\n"
+        f"Library :- Pyrogram 2.0\n"
+        f"Server :- Railway\n"
+        f"Total Renamed File :- {total_rename}\n"
+        f"Total Size Renamed :- {humanbytes(int(total_size))}",
+        quote=True
+    )
